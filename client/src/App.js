f9930ed24dc0d6242bc15723e2d6c0ca15f1c499
unknown <hebamarzouk17@gmail.com>
2022-03-16 22:32:37 +0200

    Create the products and add the data file to the working directly

--- client/src/App.js
+++ client/src/App.js
@@ -10,9 +10,18 @@ function App() {
         <div className="products">
           {data.products.map((product) => (
             <div className="product" key={product.slug}>
-              <p>{product.name}</p>
-              <p>{product.price}</p>
-              <img src={product.image} alt={product.name} />
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
             </div>
           ))}
         </div>

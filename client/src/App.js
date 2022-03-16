1a459221d83dff40329dc112de2d5926a292a291
unknown <hebamarzouk17@gmail.com>
2022-03-16 02:07:45 +0200

    Creating and styling a products list

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
+import data from "./data";
 function App() {
   return (
     <div>
       <header>
         <a href="/">amazona</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <p>{product.name}</p>
+              <p>{product.price}</p>
+              <img src={product.image} alt={product.name} />
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }

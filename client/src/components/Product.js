6fb87aa62b43052d318023aede2911bf6efce9e4
unknown <hebamarzouk17@gmail.com>
2022-03-18 02:08:22 +0200

    Create product card component and a rating component and used font awesome cdn to add the stars

---
+++ client/src/components/Product.js
@@ -0,0 +1,21 @@
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import Ratings from "./Ratings";
+export default function Product({ product }) {
+  return (
+    <Card>
+      <Link to={`/product/${product.slug}`}>
+        <img src={product.image} className="card-img-top" alt={product.name} />
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product.slug}`}>
+          <Card.Title>{product.name}</Card.Title>
+        </Link>
+        <Ratings rating={product.rating} numReviews={product.numReviews} />
+        <Card.Text>${product.price}</Card.Text>
+        <Button>Add to cart</Button>
+      </Card.Body>
+    </Card>
+  );
+}
